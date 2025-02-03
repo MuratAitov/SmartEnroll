9e5f32ec02432c626e35bb5eae32f81e12ae94ca
Murat <140990101+MuratAitov@users.noreply.github.com>
2025-02-02 18:54:22 -0800

    database setup

---
+++ database/connecting.py
@@ -0,0 +1,13 @@
+from supabase import create_client, Client
+from credentials import SUPABASE_URL,SUPABASE_KEY
+
+# Твой URL и ключ Supabase
+
+# Подключаемся к Supabase
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# Пример запроса: получить все строки из таблицы "courses"
+response = supabase.table("courses").select("*").execute()
+
+# Вывод результата
+print(response.data)
