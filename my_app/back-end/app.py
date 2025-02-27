ee061c643207c2d225d6cfe52a59c895db626685
Johanna Fan <xfan@zagmail.gonzaga.edu>
2025-02-27 12:57:21 -0800

    structuring

--- my_app/back-end/app.py
+++ my_app/back-end/app.py
@@ -1,19 +1,23 @@
-import os
 from flask import Flask, send_from_directory
+from controllers.user_controller import user_bp
+from controllers.major_controller import major_bp
+from controllers.enrollment_controller import enrollment_bp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../interface')
 
+# Register blueprints
+app.register_blueprint(user_bp, url_prefix='/user')
+app.register_blueprint(major_bp, url_prefix='/major')
+app.register_blueprint(enrollment_bp, url_prefix='/enrollment')
 
-INTERFACE_FOLDER = os.path.join(os.path.dirname(__file__), '..', 'interface')
-
+# Serve frontend files
 @app.route('/')
 def serve_index():
-    return send_from_directory(INTERFACE_FOLDER, 'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/<path:filename>')
-def serve_static_files(filename):
-    return send_from_directory(INTERFACE_FOLDER, filename)
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
